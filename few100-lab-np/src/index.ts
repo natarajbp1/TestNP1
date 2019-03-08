2bb6127f63e71cd3a8485b9925a042686d01bcbb
natarajbp1 <nataraj.poddar@gmail.com>
2019-03-08 18:41:32 +0000

    Basic tips calculator ready

--- few100-lab-np/src/index.ts
+++ few100-lab-np/src/index.ts
@@ -1,3 +1,26 @@
 import './styles.css';
-console.log('Ready to Party');
+
+const tips = document.querySelectorAll('button');
+
+tips.forEach(s => s.addEventListener('click', handleClick));
+
+function handleClick() {
+    const billInput = parseFloat((<HTMLInputElement>document.getElementById("billInput")).value);
+    const tipPercent = document.querySelector('#tipPercent');
+    const billAmount = document.querySelector('#billAmount');
+    const billTipPercent = document.querySelector('#billTipPercent');
+    const billAmountTip = document.querySelector('#billAmountTip');
+    const billAmountTotal = document.querySelector('#billAmountTotal');
+    const b = this as HTMLButtonElement;
+    tipPercent.innerHTML = b.value;
+    billAmount.innerHTML = billInput.toString();
+    billTipPercent.innerHTML = b.value;
+    const tipAmount: number = (parseFloat(b.value) / 100) * (billInput);
+    billAmountTip.innerHTML = tipAmount.toFixed(2).toString();
+    billAmountTotal.innerHTML = (tipAmount + billInput).toFixed(2).toString();
+    console.log(billInput);
+    console.log(b.value);
+}
+
+
 //Test
